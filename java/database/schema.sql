a2d3b4455588e1e11aa8e1a8e4e9d663e5ea7c15
Jordan Barnes <jordansbarnes2015@gmail.com>
2025-08-11 12:32:33 -0400

    React side now pulls the values for the select tag and brings it over to java, Just need to work on adding it all to the database and styling it now

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -7,7 +7,6 @@ DROP TABLE IF EXISTS users, movie, users_movie, genre, movie_genre, users_genre;
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
-	favorite_genre varchar(50) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
--- react/src/views/RegisterView/RegisterView.jsx
+++ react/src/views/RegisterView/RegisterView.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AuthService from '../../services/AuthService';
 import Notification from '../../components/Notification/Notification';
 
 import styles from './RegisterView.module.css';
+import { faV } from '@fortawesome/free-solid-svg-icons';
 
 export default function RegisterView() {
   const navigate = useNavigate();
@@ -14,11 +15,15 @@ export default function RegisterView() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [favoriteGenre, setFavoriteGenre] = useState('');
+  const [favoriteGenre, setFavoriteGenre] = useState([]);
+
+  function handleChange(event) {
+    const selectedValues = Array.from(event.target.selectedOptions).map(option => option.value);
+    setFavoriteGenre(selectedValues);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-
 
     // Validate the form data
     if (password !== confirmPassword) {
@@ -45,12 +50,16 @@ export default function RegisterView() {
     }
   }
 
+  useEffect(() => {
+    console.log(favoriteGenre);
+  }, [favoriteGenre]);
+
   return (
-  
+
     <div id="view-register" className={styles.container}>
       <h2 className={styles.title}>Register</h2>
       <p className={styles.description}>
-      Explore great movies, save your favorites, and get smart suggestions based on your taste. 
+        Explore great movies, save your favorites, and get smart suggestions based on your taste.
       </p>
 
       <Notification notification={notification} clearNotification={() => setNotification(null)} />
@@ -71,27 +80,33 @@ export default function RegisterView() {
           />
         </div>
 
+        <div>
+          <label htmlFor="favoriteGenre">Favorite Genres: </label>
+          <select id="favoriteGenre" name="genres" multiple onChange={handleChange}>
+            <option value="Action">Action</option>
+            <option>Adventure</option>
+            <option>Animation</option>
+            <option>Comedy</option>
+            <option>Crime</option>
+            <option>Documentary</option>
+            <option>Drama</option>
+            <option>Family</option>
+            <option>Fantasy</option>
+            <option>History</option>
+            <option>Horror</option>
+            <option>Music</option>
+            <option>Mystery</option>
+            <option>Romance</option>
+            <option>Science Fiction</option>
+            <option>TV Movie</option>
+            <option>Thriller</option>
+            <option>War</option>
+            <option>Western</option>
+          </select>
+        </div>
+
         <div className={styles.formControl}>
           <label htmlFor="password" className={styles.password}>Password:</label>
-        <div className="form-control">
-          <label htmlFor="favoriteGenre">Favorite Genre:</label>
-          <input
-            type="text"
-            id="favoriteGenre"
-            value={favoriteGenre}
-            size="50"
-            required
-            autoFocus
-            autoComplete="favoriteGenre"
-            onChange={(event) => setFavoriteGenre(event.target.value)}
-          />
-        </div>
-        </div>
-
-
-
-        <div className="form-control">
-          <label htmlFor="password">Password:</label>
           <input
             type="password"
             id="password"
@@ -118,9 +133,10 @@ export default function RegisterView() {
           Register
         </button>
         <p className={styles.registerText}>
-        Have an account? <Link to="/login"> Log-in</Link>
+          Have an account? <Link to="/login"> Log-in</Link>
         </p>
       </form>
     </div>
   );
 }
+
